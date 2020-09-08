768dad739f9f594550b40f9b66faba8e451496e5
filthyningen <malikliddell@gmail.com>
2020-09-07 21:28:50 -0400

    Added console.table. Started my add and view function. Added insert into on sql.

--- employee.js
+++ employee.js
@@ -1,4 +1,6 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -11,3 +13,37 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
 });
+
+function departmentsAdd() {
+
+};
+function rolesAdd() {
+
+}
+function employeesAdd() {
+
+}
+function departmentsView() {
+
+};
+function rolesView() {
+
+}
+function employeesView() {
+
+}
+function rolesUpdate() {
+
+}
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    },
+  ])
+  .then(function(response) {
+
+  });
--- employee.sql
+++ employee.sql
@@ -3,3 +3,26 @@ DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 
 USE employeeDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    names VARCHAR(30) 
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
+
+INSERT INTO department () 
+VALUES
